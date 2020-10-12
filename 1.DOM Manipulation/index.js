7eb33223a6da2960de21bd940a274bc2b60d95be
Divyank Kargeti <divyankkargeti99@gmail.com>
2020-10-12 23:13:11 +0530

    22. DOM Added

---
+++ 1.DOM Manipulation/index.js
@@ -0,0 +1,9 @@
+//Document Object Model
+
+//Selecting elements on the webpage - DOM Manipulation
+//2 ways: 1. Query Selector and 2. by Id
+
+// document.getElementById("mydiv");
+
+title = document.getElementByTagName('h1');
+title[0];
