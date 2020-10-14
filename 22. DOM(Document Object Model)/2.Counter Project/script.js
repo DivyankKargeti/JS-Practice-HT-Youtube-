47aaf13120d249409d18a18c7d8719f2a09f1a25
Divyank Kargeti <divyankkargeti99@gmail.com>
2020-10-15 02:10:14 +0530

    DOM Counter project --> script file added

---
+++ 22. DOM(Document Object Model)/2.Counter Project/script.js
@@ -0,0 +1,16 @@
+var counter = document.querySelector(".counter");
+
+var followers = document.querySelector(".followers");
+
+let count = 1;
+
+setInterval(() => {
+	if(count < 1000){
+		count++;
+		counter.innerText = count;
+	}
+}, 1) //after 1 milli second
+
+// setTimeout(() => {
+// 	followers.innerText = "Followers on Instagram!"; //Text to modify after 1000 number transition
+// } , 4000);//after 4 milli sec
