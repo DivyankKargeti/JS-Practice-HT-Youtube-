62ad85948ce43b617da1068959b244d1bb5d252a
Divyank Kargeti <divyankkargeti99@gmail.com>
2020-11-17 03:24:07 +0530

    modified

---
+++ 29. scope.js
@@ -0,0 +1,10 @@
+//scope in JS
+
+//In{} other langs. like cpp, jaa, python ect. 
+//these consider '{}' as scope but in JS scope means something else....
+//JS scope menas a 'function'
+
+
+//===>> var
+//===> let
+//===> const
