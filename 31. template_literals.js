20f08e12983f1473135e06ce05f6d8ee783545d4
DivyankKargeti <divyankkargeti99@gmail.com>
2020-11-29 00:34:28 +0530

    modified

---
+++ 31. template_literals.js
@@ -0,0 +1,16 @@
+//Template Literals for Strings in JS
+
+
+var fname = "Divyank";
+var lname = "Kargeti";
+var clg = "UPES";
+var branch = "IOT";
+var cgpa = "8";
+
+//template literals intro
+
+console.log(`Hi my name is ${fname} ${lname}`);
+
+console.log(`college: ${clg} located in Dehradun`);
+
+console.log(`my branch is ${branch} and cgpa is: ${cgpa}`);
